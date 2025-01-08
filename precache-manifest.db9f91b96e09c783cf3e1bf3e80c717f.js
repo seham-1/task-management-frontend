cf372b045c563dba48fe85ed8c77cbd175a94fa8
seham-1 <seham-1@users.noreply.github.com>
2025-01-08 10:09:34 +0000

    deploy: 4b52e2bc5140bf6d41442fb45c3bf983462733c9

---
+++ precache-manifest.db9f91b96e09c783cf3e1bf3e80c717f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51e6fafd5cd6f192e099b22a27bd49c9",
+    "url": "https://seham-1.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "780fe6ca14880450d023",
+    "url": "https://seham-1.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "e70e95b5079f112deee0",
+    "url": "https://seham-1.github.io/task-management-frontend/static/js/2.d547dce9.chunk.js"
+  },
+  {
+    "revision": "780fe6ca14880450d023",
+    "url": "https://seham-1.github.io/task-management-frontend/static/js/main.cf47ee39.chunk.js"
+  },
+  {
+    "revision": "b47675ec55870f3b4851",
+    "url": "https://seham-1.github.io/task-management-frontend/static/js/runtime~main.14bee567.js"
+  }
+]);
